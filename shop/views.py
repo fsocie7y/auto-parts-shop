6ae83cf16f73c577460d4c8c2cf107a41ee6e75d
Adrian Kuzhel <gamelooc123@gmail.com>
2022-11-10 23:03:35 +0200

    Home page was added

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """View function for the home page of the site."""
+
+    return render(request, "shop/index.html")
