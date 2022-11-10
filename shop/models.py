997bf941c1ec2caeb9ea3f52fd0a2f420c6e70fc
Adrian Kuzhel <gamelooc123@gmail.com>
2022-11-10 21:59:03 +0200

    Models and migrations was creadted

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
-# Create your models here.
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    country = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name} {self.country}"
+
+
+class AutoPart(models.Model):
+    part_name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    manufacturer = models.ForeignKey(to=Manufacturer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.part_name}, price: ({self.price})"
+
+
+class Customer(AbstractUser):
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Order(models.Model):
+    order_number = models.IntegerField(auto_created=True)
+    auto_parts = models.ManyToManyField(to=AutoPart, related_name="orders")
+    customers = models.ManyToManyField(to=Customer, related_name="orders")
+
+    def __str__(self):
+        return f"Order number: {self.order_number}"
